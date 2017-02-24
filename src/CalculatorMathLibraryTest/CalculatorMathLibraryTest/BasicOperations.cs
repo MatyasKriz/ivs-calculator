9537817598a555e06872659967b20d7d482228e8
Matyáš Kříž <kriz.matyas8.10@gmail.com>
2017-02-24 18:22:15 +0100

    Add simple tests, issue #2

---
+++ src/CalculatorMathLibraryTest/CalculatorMathLibraryTest/BasicOperations.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace MathLibraryTesting
+{
+    [TestFixture]
+    public class BasicOperations
+    {
+        #region Simple Tests Testing Basic Operations
+        [Test]
+        public void Adding()
+        {
+            Assert.That(1 + 1 == 2);
+            Assert.That(100 + 100 == 200);
+            Assert.That(1000 + 1000 == 2000);
+        }
+
+        [Test]
+        public void Subtracting()
+        {
+            Assert.That(1 - 1 == 0);
+            Assert.That(100 - 50 == 50);
+            Assert.That(1000 - 900 == 100);
+        }
+
+        [Test]
+        public void Multiplying()
+        {
+            Assert.That(1 * 1 == 1);
+            Assert.That(10 * 10 == 100);
+            Assert.That(13 * 1000 == 13000);
+        }
+
+        [Test]
+        public void Dividing()
+        {
+            Assert.That(1 / 1 == 1);
+            Assert.That(100 / 1 == 100);
+            Assert.That(2000 / 1000 == 2);
+        }
+        #endregion
+    }
+}
