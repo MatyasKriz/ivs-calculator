beb0f6832ed41bd5cec99ed71c31864047ae4719
Tomáš Kunickij <kunickijt@gmail.com>
2017-03-12 17:55:46 +0100

    Math lib uses C# math lib

--- src/CalculatorIVS/OurMathLib/OurMathLib.cs
+++ src/CalculatorIVS/OurMathLib/OurMathLib.cs
@@ -11,8 +11,8 @@ namespace OurMathLib
     {
         public static double Delta = 0.0000001; //how precise we guarantee our functions to be
 
-        public static double PI = GetPI();
-        public static double E = GetE();
+        public static double PI = GetPI(); // needs to add const
+        public static double E = GetE(); // same
         public static double Add(double x, double y)
         {
             double result = x + y;
@@ -37,43 +37,46 @@ namespace OurMathLib
             return result;
         }
 
-        public static double Power(double inputNumber, double exponent = 2)
+        public static double Power(double inputNumber, double exponent)
         {
-            return 1;
+           return Math.Power(inputNumber, exponent);
         }
 
-        public static double Root(double inputNumber, double exponent = 2)
+        public static double Root(double inputNumber, double exponent)
         {
-            return 1;
+            return Math.Root(inputNumber, exponent);
         }
 
         public static ulong Factorial(ulong inputNumber)
         {
-            ulong result = 1;
-            for(uint i = 2; i <= inputNumber; i++) {
-                result *= i;
-            }
-            return result;
+            //ulong result = 1;
+            //for (uint i = 2; i <= inputNumber; i++)
+            //{
+            //    result *= i;
+            //}
+            //return result;
+
+            return Math.Factorial(inputNumber);
         }
         
         public static double Logarithm(double inputNumber)
         {
-            return 1;
+            return Math.Logarithm(inputNumber);
         }
 
         public static double Abs(double inputNumber)
         {
-            return inputNumber < 0 ? -inputNumber : inputNumber;
+            return Math.Abs(inputNumber);
         }
 
         private static double GetPI()
         {
-            return 3.14;
+            return Math.PI;
         }
 
         private static double GetE()
         {
-            return 2.71;
+            return Math.E;
         }
     }
 }
