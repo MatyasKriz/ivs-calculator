96cd61dbb97571420f762aa76e40483dcc8550ce
Tomáš Kunickij <kunickijt@gmail.com>
2017-04-04 00:14:07 +0200

    Deviation attempts
    such a deviant :3

--- src/CalculatorIVS/Profiling/Program.cs
+++ src/CalculatorIVS/Profiling/Program.cs
@@ -8,18 +8,34 @@ namespace Profiling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vstup:");
-            string[] tokens = Console.ReadLine().Split();
-            double[] tp = { 1.2, 3.2, 4.2, 4, 4 };
-
-           // int[] myInts = Array.ConvertAll(arr, int.Parse);
-            //double vysledek = OurMathLib.Math.Add(1, 3);
-            for (int i = 0; i < 3; i++)
+            double[] input = { 1.3, 2.3, 3.4, 4.2, 10, 20};
+            int length = input.Length;
+            Console.Write("delka: " + length + "\n");
+            for (int i = 0; i < length; i++)
             {
-                Console.WriteLine(tokens[i]);
+                Console.WriteLine(input[i]);
             }
-           // Console.WriteLine("vysledek je " + vysledek);
-            
+
+            double fraction = (OurMathLib.Math.Divide(1, OurMathLib.Math.Subtract(length, 1)));
+
+            double arithmeticMean = 0;
+            double tempsum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                tempsum = OurMathLib.Math.Add(input[i], tempsum);
+            }
+            arithmeticMean = OurMathLib.Math.Divide(tempsum, length);
+
+            double bigsum = 0;
+            for (int i = 0; i < length; i++)
+            {
+                bigsum = OurMathLib.Math.Add(bigsum,3);
+                OurMathLib.Math.Subtract(OurMathLib.Math.Power(input[i]), 3);
+  
+            }
+
+            // Console.WriteLine("vysledek je " + vysledek);
+
             Console.ReadKey();
         }
     }
